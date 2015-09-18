2368e13c644e2a826a0f6396f5ee0b9cb57e673f
chacha <ckalstn93@gmail.com>
2015-09-18 17:21:11 +0900

    change program1.c

--- homework2/program1.c
+++ homework2/program1.c
@@ -7,35 +7,35 @@ int main()
 {
 
 	int top_num;
-	clock_t start,finish;
+	clock_t start, finish;
 	double duration;
 	int count = 0;
 
 	printf("몇층의 탑인가요?");
-	scanf("%d",&top_num);
+	scanf("%d", &top_num);
 
 	start = clock();
-	hanoi_top(top_num,'A','B','C',&count);
+	hanoi_top(top_num, 'A', 'B', 'C', &count);
 	finish = clock();
 
 	printf("\n");
 
-	duration = ((double)(finish - start)/CLOCKS_PER_SEC);
-	printf("이동횟수는 %d 회 입니다 \n",count);
-	printf("실행시간은 %.6f 초 입니다.\n",duration);
+	duration = ((double)(finish - start) / CLOCKS_PER_SEC);
+	printf("이동횟수는 %d 회 입니다 \n", count);
+	printf("실행시간은 %.6f 초 입니다.\n", duration);
 
 }
 void hanoi_top(int number, char start, char temp, char finish, int* count)
 {
-	
 
-	if(number == 1)
-		printf("A -> C\t");
+
+	if (number == 1)
+		printf("%c -> %c\t", start, finish);
 	else
 	{
-		hanoi_top(number-1,start,finish,temp,count);
-		printf("%c -> %c\t",start,finish);
-		hanoi_top(number-1,temp,start,finish,count);
+		hanoi_top(number - 1, start, finish, temp, count);
+		printf("%c -> %c\t", start, finish);
+		hanoi_top(number - 1, temp, start, finish, count);
 	}
 	*count = *count + 1;
 }
